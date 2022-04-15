4f0a8ada9ca712e3b04fccbc0881ba75f9537450
Erick Avalos <erick@Ericks-MBP.lan>
2022-04-14 17:59:22 -0700

    inital commit

---
+++ config.js
@@ -0,0 +1,6 @@
+const blockfrostApiKey = {
+    0: "testnetQZrEnO7ebklQxHYy1W7pIjsV4fArzfYF", // testnet
+    // 1: "mainnetcGJC6RhtYh7feNYqO9tFBA0bnFDPEKai" // mainnet
+}
+
+export default blockfrostApiKey;
